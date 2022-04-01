45cb303928cc44dc3797a174a1f2db762360e410
thomasguy <twguy66@gmail.com>
2022-04-01 22:00:20 +0100

    dashboard

--- studio/src/dashboardConfig.js
+++ studio/src/dashboardConfig.js
@@ -1,14 +1,15 @@
 export default {
   widgets: [
     {
-      name: 'AmplifyDeployButton',
+      name: 'Amplify-Deploy-Button',
       options: {
         webhookUrls: [
           'https://webhooks.amplify.eu-west-2.amazonaws.com/prod/webhooks?id=2f421055-e4cd-44d8-8b53-8c9b71883ab3&token=0qswXgnG4222pOyxR4k7vCmUrHjlSyl78mGvNiM',
         ],
         title: 'Deploy content changes',
-        buttonText: 'Deploy',
+        buttonText: 'Deploy to Amplify',
       },
+      layout: { width: 'medium' },
     },
   ],
 };
